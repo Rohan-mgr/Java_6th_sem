83dbfab4f52c8deaa629cc9d8baee87e93b38b5a
Rohan Magar <rohan.magar.415@gmail.com>
2023-02-27 20:23:39 +0545

    added looping for guessing

--- Assignment03/Assignment03.java
+++ Assignment03/Assignment03.java
@@ -3,21 +3,24 @@ import java.lang.Math;
 
 public class Assignment03 {
     public static void main(String[] args){
-        double randomNo = Math.floor((Math.random() * 10) + 1);
 
         Scanner inputObj = new Scanner(System.in);
-        System.out.println("Guess the number between 1-10:");
+        int turn = 5;
+        for(int i=5; i>0; i--){
+            double randomNo = Math.floor((Math.random() * 10) + 1);
+            System.out.println("Guess the number between 1-10:");
+            double userInput = inputObj.nextDouble();
 
-        double userInput = inputObj.nextDouble();
+            System.out.println("\nGame Result: ");
+            System.out.println("System Generated Random No: "+randomNo);
+            System.out.println("User Input No: "+userInput + "\n");
 
-        System.out.println("\nGame Result: ");
-        System.out.println("System Generated Random No: "+randomNo);
-        System.out.println("User Input No: "+userInput + "\n");
-
-        if(randomNo == userInput) {
-            System.out.println("Hurray! you guess the correct number.");
-        } else {
-            System.out.println("Sorry! your guess was wrong\n");
+            if(randomNo == userInput) {
+                System.out.println("Hurray! you guess the correct number.\n");
+                break;
+            } else {
+                System.out.println("Sorry! your guess was wrong. Try Again(Turn left: " + (--turn) + ")\n");
+            }
         }
     }
 }
